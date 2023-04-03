723163d100fe4710fa1921c93da66a418c1c281c
shaochengbao <samge720@163.com>
2023-04-03 22:40:16 +0800

    update

--- main.go
+++ main.go
@@ -1,43 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	client := http.Client{}
+	client := &http.Client{}
+
 	targetURL := "https://api.openai.com" // 替换为要转发的域名
-	modifiedRequest, _ := http.NewRequest(http.MethodPost, targetURL, r.Body)
-	modifiedRequest.Header = r.Header
-	resp, err := client.Do(modifiedRequest)
+	req, _ := http.NewRequest(r.Method, targetURL, r.Body)
+	req.Header = r.Header
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatalf("Error sending request to target URL: %v", err)
 	}
-	defer resp.Body.Close()
-
-	w.Header().Add("Content-Type", "application/octet-stream")
-	for {
-		body, err := io.ReadAll(resp.Body)
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
 		if err != nil {
-			log.Fatalf("Error reading response body: %v", err)
-		}
 
-		if len(body) == 0 {
-			break
 		}
+	}(resp.Body)
 
-		_, err = w.Write(body)
-		if err != nil {
-			log.Fatalf("Error writing response to client: %v", err)
-		}
+	for key, value := range resp.Header {
+		w.Header().Set(key, value[0])
+	}
+	w.WriteHeader(resp.StatusCode)
 
-		w.(http.Flusher).Flush()
+	if _, err := io.Copy(w, resp.Body); err != nil {
+		log.Fatalf("Error copying response to client: %v", err)
 	}
 }
-
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Starting server on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
+	}
 }
